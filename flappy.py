dc07732acb30eb2a79cf6479de35958ca474196c
Banji <khanjani1997@gmail.com>
2020-11-08 22:14:32 +0330

    Added new files, modules and directory that I made before

--- flappy.py
+++ flappy.py
@@ -9,7 +9,7 @@ def infinity_floor():
 screen = pygame.display.set_mode((576, 950))
 clock = pygame.time.Clock()
 
-
+ 
 
 
 background_surface = pygame.image.load("assets/background-day3.png").convert()
