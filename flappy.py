4da4ff9e77978f2203f0d78a94a83d1177c9654e
Banji <khanjani1997@gmail.com>
2020-11-09 13:04:12 +0330

    Added some new features and comment on functions and variables

--- flappy.py
+++ flappy.py
@@ -1,33 +1,39 @@
 import pygame, sys
 
 pygame.init()
+#! Game Functions
+# Putting two floor_surface next to each other
 def infinity_floor():
     screen.blit(floor_surface, (floor_x_position, 740))
     screen.blit(floor_surface, (floor_x_position + 576, 740))
 
 
 
+# Display width and height
 screen = pygame.display.set_mode((576, 950))
 clock = pygame.time.Clock()
 
-#! Game variables
+
+
+
+#! Game Variables
 gravity = 0.15
 bird_movement = 0
 
 
-
+# Background-Day surface
 background_surface = pygame.image.load("assets/background-day3.png").convert()
 background_surface = pygame.transform.scale2x(background_surface)
 
 
 
-
+# Floor Surface
 floor_surface = pygame.image.load("assets/base.png").convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_position = 0
 
 
-
+# Bird Red
 bird_surface = pygame.image.load("assets/redbird-upflap.png").convert()
 bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rectangle = bird_surface.get_rect(center=(100, 475))
@@ -42,6 +48,7 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
+                bird_movement = 0
                 bird_movement -= 8
         
         
